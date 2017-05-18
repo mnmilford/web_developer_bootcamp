7783b18d926abf37fc69ff440090abd39678b7ca
Michael Milford <mnmilford@gmail.com>
2017-05-18 18:24:32 -0500

    Implement annoying while loop until users answers 'yes'

--- javascript_control_flow/annoyomatic.js
+++ javascript_control_flow/annoyomatic.js
@@ -1,5 +1,10 @@
-// The Annoy-omatic 3000
+// The Annoy-O-Matic 3000
 // 1. Ask the user "Are we there yet?"
 // 2. Keep asking again and again until they enter "yes"
 // 3. Then, alert "Yay, we finally made it!"
 
+var answer = ""; 
+while (answer !== "yes") {
+  answer = prompt("Are we there yet?").toLowerCase();
+}
+alert("Yay, we finally made it!");
