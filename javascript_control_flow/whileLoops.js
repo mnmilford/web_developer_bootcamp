d293b16013edbdc6683b62c4f2520158bfc98eb2
Michael Milford <mnmilford@gmail.com>
2017-05-18 17:54:43 -0500

    Write a while loop to count from -10 to 19

---
+++ javascript_control_flow/whileLoops.js
@@ -0,0 +1,13 @@
+// Print all numbers between -10 & 19
+
+var i = -10;
+while (i < 19) {
+  console.log(i);
+  i++;
+}
+
+// Print all even numbers between 10 & 40
+
+// Print all odd numbers between 300 and 333
+
+// Print all numbers divisible by 5 AND 3 between 5 and 50
