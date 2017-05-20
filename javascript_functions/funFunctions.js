7379678c48490ab7ea3b203d52f46f18cd3feb73
Michael Milford <mnmilford@gmail.com>
2017-05-19 23:49:31 -0500

    Create isEven() function using ternary operator

---
+++ javascript_functions/funFunctions.js
@@ -0,0 +1,10 @@
+// Write a function which takes a single numeric argument and 
+// returns true if the number is even, and false otherwise
+
+function isEven(num) {
+  return num % 2 === 0 ? true : false;
+}
+
+console.log(isEven(4));
+console.log(isEven(21));
+console.log(isEven(68));
