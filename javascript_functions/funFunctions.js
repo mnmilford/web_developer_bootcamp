c424c61a23111034887890ccf5d11ec4e1d4e59d
Michael Milford <mnmilford@gmail.com>
2017-05-20 00:10:01 -0500

    Create a factorial() function

--- javascript_functions/funFunctions.js
+++ javascript_functions/funFunctions.js
@@ -5,6 +5,22 @@ function isEven(num) {
   return num % 2 === 0 ? true : false;
 }
 
-console.log(isEven(4));
-console.log(isEven(21));
-console.log(isEven(68));
+// console.log(isEven(4));
+// console.log(isEven(21));
+// console.log(isEven(68));
+
+
+// Write a factorial() function which takes a single number
+// and returns the factorial of that number
+
+function factorial(num) {
+  if (num === 0) {return 1};
+  for (var i = num; i > 1; i--) {
+        num *= i-1;
+      }
+  return num
+}
+
+// console.log(factorial(5));
+// console.log(factorial(0));
+// console.log(factorial(10));
