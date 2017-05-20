6cdac397440e03eb04ff5515ffb54d43730ce6f7
Michael Milford <mnmilford@gmail.com>
2017-05-20 15:55:04 -0500

    Add option for user to remove item from todo list

--- javascript_arrays/console_todo_list.js
+++ javascript_arrays/console_todo_list.js
@@ -1,6 +1,14 @@
 var action;
 var todoList = [];
 
+function arrayToList(arr) {
+  var str = ""
+  for (var i = 0; i < arr.length; i++) {
+    str += [i+1] + ". " + arr[i] + "\n";
+  }
+  return str;
+}
+
 while (true) {
   action = prompt('Welcome to the Todo List app. Please enter a command.').toLowerCase();
   if (action === 'quit') {
@@ -9,7 +17,10 @@ while (true) {
       console.log(todoList);
   } else if (action === 'new') {
       todoList.push(prompt('What would you like to do?'));
+  } else if (action === 'remove') {
+      var removeItem = prompt('Which item would you like to remove? Enter a number.\n' + arrayToList(todoList));
+      todoList.splice(removeItem - 1);
   } else {
-      alert('I don\'t understand that command. Try "new", "list", or "quit."');
+      alert('I don\'t understand that command. Try "new", "remove", "list", or "quit."');
   }
 }
