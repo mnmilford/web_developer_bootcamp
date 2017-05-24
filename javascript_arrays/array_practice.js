0a2a15131852f2b950dd225423f6700130471d72
Michael Milford <mnmilford@gmail.com>
2017-05-24 14:12:29 -0500

    Create isUniform() function to verify all items in array have same value

--- javascript_arrays/array_practice.js
+++ javascript_arrays/array_practice.js
@@ -1,6 +1,6 @@
 // Write a function printReverse() that takes an array as an argument and prints out the elements in the array in reverse order (don\'t modify the original array)
 
-arr = [1, 2, 3, 4, 5];
+var arr = [1, 2, 3, 4, 5];
 
 function printReverse(array) {
   for (var i = array.length-1; i >= 0; i--) {
@@ -8,4 +8,22 @@ function printReverse(array) {
   }
 }
 
-printReverse(arr);
+// printReverse(arr);
+
+// Write a function isUniform() which takes an array as an argument and returns true if all elements are identical
+
+function isUniform(array) {
+  baseItem = array[0];
+  for (var i = 1; i < array.length; i++) {
+    if (baseItem !== array[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+console.log(isUniform(arr));  //false
+console.log(isUniform([1,1,1,2]));  //false
+console.log(isUniform([3,3,3,3,3,3,3]));  //true
+console.log(isUniform(["a", "b", "p"]));  //false
+console.log(isUniform(["b", "b", "b"]));  //true
