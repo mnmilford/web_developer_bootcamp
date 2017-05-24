9838f54d6ac7ca0146e4a7be1f4a93cbde29683a
Michael Milford <mnmilford@gmail.com>
2017-05-24 13:30:03 -0500

    Modify 'list' command to alert() list and console.log list

--- javascript_arrays/console_todo_list.js
+++ javascript_arrays/console_todo_list.js
@@ -2,10 +2,10 @@ var action;
 var todoList = [];
 
 function arrayToList(arr) {
-  var str = ""
-  for (var i = 0; i < arr.length; i++) {
+  var str = "***** Todo List *****\n"
+  arr.forEach(function(todo, i) {
     str += [i+1] + ". " + arr[i] + "\n";
-  }
+  });
   return str;
 }
 
@@ -14,14 +14,12 @@ while (true) {
   if (action === 'quit') {
     break;
   } else if (action === 'list') {
-      console.log("***** Todo List *****");
-      todoList.forEach(function(todo, i) {
-        console.log(i+1 + ": " + todo);
-      });
+      console.log(arrayToList(todoList));
+      alert(arrayToList(todoList));
   } else if (action === 'new') {
       todoList.push(prompt('What would you like to do?'));
   } else if (action === 'remove') {
-      var removeItem = prompt('Which item would you like to remove? Enter a number.\n' + arrayToList(todoList));
+      var removeItem = prompt('Which item would you like to remove? Enter a number.\n\n' + arrayToList(todoList));
       todoList.splice(removeItem - 1, 1);
   } else {
       alert('I don\'t understand that command. Try "new", "remove", "list", or "quit."');
