ced306b7a344dc5d58671f7a4dec47834767e453
Michael Milford <mnmilford@gmail.com>
2017-05-24 19:30:03 -0500

    Add for...of loop to log various movie properties

--- javascript_objects/movieDB.js
+++ javascript_objects/movieDB.js
@@ -3,20 +3,28 @@ let movies = [
     title: "Holes",
     rating: 2.5,
     watched: true
-  }
+  },
   {
     title: "Fast and Furious, Tokyo Drift",
     rating: 3,
     watched: true
-  }
+  },
   {
     title: "Fast10: Your Seatbelts",
     rating: "n/a",
     watched: false
-  }
+  },
   {
     title: "Moonlight",
     rating: 4.5,
     watched: false
   }
 ]
+
+for (let movie of movies) {
+  let str = "";
+  movie.watched ? str += "You have watched " : str += "You have not seen ";
+  str += '"' + movie.title + '" - ';
+  str += movie.rating + " stars";
+  console.log(str);
+}
