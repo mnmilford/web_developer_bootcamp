e81eecffbbbca409e0c459dacc3f6749aaf939a7
Michael Milford <mnmilford@gmail.com>
2017-05-24 19:21:14 -0500

    Create movieDB file with array of movies with various properties

---
+++ javascript_objects/movieDB.js
@@ -0,0 +1,22 @@
+let movies = [
+  {
+    title: "Holes",
+    rating: 2.5,
+    watched: true
+  }
+  {
+    title: "Fast and Furious, Tokyo Drift",
+    rating: 3,
+    watched: true
+  }
+  {
+    title: "Fast10: Your Seatbelts",
+    rating: "n/a",
+    watched: false
+  }
+  {
+    title: "Moonlight",
+    rating: 4.5,
+    watched: false
+  }
+]
