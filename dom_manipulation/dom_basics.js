3409b7df5b144109d57bca51b0007f973cbbbed2
Michael Milford <mnmilford@gmail.com>
2017-05-25 19:33:26 -0500

    Add script to change the src attribute of an img

--- dom_manipulation/dom_basics.js
+++ dom_manipulation/dom_basics.js
@@ -15,7 +15,10 @@ var ul = document.querySelector('ul');
 ul.innerHTML += '<li>Roses</li>';
 
 // Selecting and remove the first img tag after 7 seconds
+// Change the second img to an entirely different image
 var images = document.getElementsByTagName('img');
 setTimeout(function(){
-  images[0].remove();
+  images[0].remove(); // This removes the first image
+  // Since the first image was removed, the second image is now at index 0
+  images[0].setAttribute('src', 'https://img.buzzfeed.com/buzzfeed-static/static/2016-07/20/11/campaign_images/buzzfeed-prod-fastlane02/16-corgi-mixes-that-will-melt-your-cold-unloving--2-2759-1469026977-1_dblbig.jpg');
 }, 7000);
