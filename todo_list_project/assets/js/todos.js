94b21c601c14d7fe7725b05d8dd9544baf204cd3
Michael Milford <mnmilford@gmail.com>
2017-06-02 21:43:18 -0500

    Add script to fade & then remove todos

--- todo_list_project/assets/js/todos.js
+++ todo_list_project/assets/js/todos.js
@@ -2,3 +2,11 @@
 $("li").on("click", function(){
   $(this).toggleClass('completed');
 });
+
+// Fade & Remove an todo item entirely when "X" is clicked
+$("li span").click(function(event){
+  $(this).parent().fadeOut(200, function(){
+    $(this).remove();
+  });
+  event.stopPropagation();
+});
