4fd7782337c6a0055311848921c00fe508353301
Michael Milford <mnmilford@gmail.com>
2017-06-02 22:55:47 -0500

    Add script that appends new todos to list

--- todo_list_project/assets/js/todos.js
+++ todo_list_project/assets/js/todos.js
@@ -1,12 +1,21 @@
 // Strikeout todo items when clicked on
-$("li").on("click", function(){
+$("ul").on("click", "li", function(){
   $(this).toggleClass('completed');
 });
 
 // Fade & Remove an todo item entirely when "X" is clicked
-$("li span").click(function(event){
+$("ul").on("click", "span", function(event){
   $(this).parent().fadeOut(200, function(){
     $(this).remove();
   });
   event.stopPropagation();
 });
+
+// Add value of input as a new list item when Enter is pressed
+$("input").keypress(function(event){
+  if (event.which === 13) {
+    let todoText = $(this).val();
+    $("ul").append("<li><span>X</span> " + todoText + "</li>");
+    $(this).val("");
+  }
+}); 
